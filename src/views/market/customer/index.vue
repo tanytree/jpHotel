6c995d9b62e6b6425df47c1c31a3a8dac57d9b28
jinpujin <shhjp10_18@163.com>
2020-11-14 11:49:14 +0800

    update

--- src/views/market/customer/index.vue
+++ src/views/market/customer/index.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-import {mapMutations} from 'Vuex';
+import {mapMutations} from 'vuex';
 import member from "./member";
 import history from "./history";
 import guest from "./guest";
