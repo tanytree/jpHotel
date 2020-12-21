1a25834e9eae3d16e5b7319a642bb803839c239d
john <376279457@qq.com>
2020-12-21 23:14:41 +0800

    update jiaoban

--- src/views/manager/index/shiftover/c1.vue
+++ src/views/manager/index/shiftover/c1.vue
@@ -9,107 +9,88 @@
   <!-- 统一的列表格式 -->
   <div class="shiftOver">
     <el-row>
-        <el-button v-for="(item,index) in tabs" :key="index"  :type="tabCurr == item.handoverType ? 'primary' : ''">
+        <el-button v-for="(item,index) in tabs" :key="index"  :type="tabCurr == item.handoverType ? 'primary' : ''" @click="changeTabs(item.handoverType)">
         {{$t('manager.typeNameList.'+item.handoverType)}}
-
         </el-button>
     </el-row>
-    <el-row>
+    <el-row style="padding-top: 20px;">
       <h3>预交班基本信息</h3>
       <el-row>
         <el-col :span="12">
           <div class="item">
-            班次时间：2020/04/20 06:01:03 至 2020/04/20 16:10:00
+            班次时间：{{info.workStarTime}} 至 {{info.workEndTime}}
           </div>
         </el-col>
         <el-col :span="12">
-          <div class="item">班次号：2020-04-20-1</div>
+          <div class="item">班次号：{{info.handoveNum}}</div>
         </el-col>
         <el-col :span="12">
           <div class="item">交班操作员：张三</div>
         </el-col>
         <el-col :span="12">
-          <div class="item">当前模式：应收模式</div>
+            <div class="item">
+              {{$t('manager.hp_patternsSuccession')}}：
+              <span v-if="info.handoverStatus == 1">{{$t('manager.hp_cashModel')}}</span>
+              <span v-if="info.handoverStatus == 2">{{$t('manager.hp_paidModel')}}</span>
+              <span v-if="info.handoverStatus == 3">{{$t('manager.hp_accountsModel')}}</span>
+            </div>
         </el-col>
       </el-row>
     </el-row>
     <el-divider></el-divider>
     <el-row>
       <h3>本班账务汇总信息</h3>
-      <div class="total">收入总计：10000.00日元</div>
+      <div class="total">收入总计：{{info.income}}日元</div>
       <el-row class="padding-tb-10">
-        <el-col :span="4">
-          <div class="item">房费：<span class="red">8000.00</span>日元</div>
+
+
+        <el-col :span="4" v-for="(item,index) in info.orderPriceProjectList">
+          <div class="item">{{$t('manager.priceType.'+item.priceType)}}：<span class="red">{{item.total}}</span>日元</div>
         </el-col>
-        <el-col :span="4">
-          <div class="item">
-            迷你吧商品：<span class="red">8000.00</span>日元
-          </div>
-        </el-col>
-        <el-col :span="4">
-          <div class="item">
-            会员卡卡费：<span class="red">8000.00</span>日元
-          </div>
-        </el-col>
-        <el-col :span="4">
-          <div class="item">其他：<span class="red">8000.00</span>日元</div>
-        </el-col>
+
       </el-row>
-      <div class="total">结算总计：6000.00日元</div>
+      <div class="total">结算总计：{{info.settlement}}日元</div>
       <el-row class="padding-tb-10">
-        <el-col :span="3">
-          <div class="item">{{$t('desk.charge')}}：<span class="blue">8000.00</span>日元</div>
-        </el-col>
-        <el-col :span="3">
-          <div class="item">现金：<span class="blue">8000.00</span>日元</div>
-        </el-col>
-        <el-col :span="3">
-          <div class="item">银行卡：<span class="blue">8000.00</span>日元</div>
-        </el-col>
-        <el-col :span="3">
-          <div class="item">微信：<span class="blue">8000.00</span>日元</div>
-        </el-col>
-        <el-col :span="3">
-          <div class="item">支付宝：<span class="blue">8000.00</span>日元</div>
-        </el-col>
-        <el-col :span="3">
-          <div class="item">其它：<span class="blue">8000.00</span>日元</div>
+        <el-col :span="4" v-for="(item,index) in info.orderPayTypeList">
+          <div class="item">{{$t('manager.payType.'+item.payType)}}：<span class="blue">{{item.total}}</span>日元</div>
         </el-col>
       </el-row>
     </el-row>
     <el-row>
       <h3>
-        本班现金上交&nbsp;&nbsp;<el-tag size="mini"
-          >当前交班模式：应收模式</el-tag
-        >
+        本班现金上交&nbsp;&nbsp;
+        <el-tag size="mini">当前交班模式：
+            <span v-if="info.handoverStatus == 1">{{$t('manager.hp_cashModel')}}</span>
+            <span v-if="info.handoverStatus == 2">{{$t('manager.hp_paidModel')}}</span>
+            <span v-if="info.handoverStatus == 3">{{$t('manager.hp_accountsModel')}}</span>
+        </el-tag>
       </h3>
       <div class="handInCash margin-tb-10">
         <el-row class="padding-tb-10">
-          <el-col :span="4" class="li">
-            <div class="item">上班现金留存：-9900.00</div>
-            <div class="item">本班现金上交：-100.00</div>
-            <div class="item">本班现金留存：-10000.00</div>
+          <el-col :span="12" class="li">
+            <!-- <div class="item">上班现金留存：{{info.upMoneyRetained}}</div> -->
+            <div class="item">本班现金上交：{{info.nowMoneyHandin}}</div>
+            <div class="item">本班现金留存：{{info.nowMoneyRetained}}</div>
+          </el-col>
+          <!-- <el-col :span="4" class="li">
+            <div class="item">上班微信留存：{{info.upWeixinRetained}}</div>
+            <div class="item">本班微信上交：{{info.upWeixinRetained}}</div>
+            <div class="item">本班微信留存：{{info.upWeixinRetained}}</div>
           </el-col>
           <el-col :span="4" class="li">
-            <div class="item">上班微信留存：0.00</div>
-            <div class="item">本班微信上交：100.00</div>
-            <div class="item">本班微信留存：100.00</div>
-          </el-col>
-          <el-col :span="4" class="li">
-            <div class="item">上班支付宝留存：0.00</div>
-            <div class="item">本班支付宝上交：100.00</div>
-            <div class="item">本班支付宝留存：100.00</div>
-          </el-col>
-          <el-col :span="4" class="li">
-            <div class="item">上班银行卡留存：0.00</div>
-            <div class="item">本班银行卡上交：100.00</div>
-            <div class="item">本班银行卡留存：100.00</div>
+            <div class="item">上班支付宝留存：{{info.upAliRetained}}</div>
+            <div class="item">本班支付宝上交：{{info.upAliRetained}}</div>
+            <div class="item">本班支付宝留存：{{info.upAliRetained}}</div>
+          </el-col> -->
+          <el-col :span="12" class="li">
+            <div class="item">本班信用卡上交：100.00</div>
+            <div class="item">本班信用卡留存：100.00</div>
           </el-col>
         </el-row>
-        <el-row class="ftRow">本班走结订单金额：6000.00</el-row>
+        <!-- <el-row class="ftRow">本班走结订单金额：{{info.goSettlement}}</el-row> -->
       </div>
     </el-row>
-    <el-row>
+    <!-- <el-row>
       <h3>本班报表</h3>
       <el-row>
         <el-col :span="4">
@@ -119,34 +100,40 @@
         </el-col>
         <el-col :span="4">
           <div class="item">
-            交班报表(按收银员)&nbsp; &nbsp;<el-button size="mini"
-              >预览</el-button
-            >
+            交班报表(按收银员)&nbsp; &nbsp;<el-button size="mini">预览</el-button>
           </div>
         </el-col>
       </el-row>
-    </el-row>
-    <el-divider></el-divider>
-    <el-row style="text-align: center">
-      <elForm inline size="small">
-<!--        <el-form-item :label="$t('commons.checkInTypeDesc')">-->
-<!--          <el-select v-model="form.text">-->
-<!--            <el-option label="正常" value="1"></el-option>-->
-<!--            <el-option label="不正常" value="2"></el-option>-->
-<!--          </el-select>-->
-<!--        </el-form-item>-->
-        <el-form-item label="">
-          <el-button type="primary" class="submit">{{ $t('commons.confirm') }}</el-button>
-        </el-form-item>
-      </elForm>
-    </el-row>
+    </el-row> -->
+    <el-row style="text-align: center;margin-top: 20px;">
+        <el-divider></el-divider>
+        <el-form inline label-width="150px" style="padding-top: 30px;">
+            <el-form-item :label="$t('food.shift.selectOnDutyer')">
+                <el-select v-model="form.handoveEmployeedId">
+                    <el-option
+                      v-for="item in employeeList"
+                      :key="item.id"
+                      :label="item.userName"
+                      :value="item.id"
+                      >
+                    </el-option>
+                </el-select>
+            </el-form-item>
+            <el-form-item :label="$t('food.shift.password')">
+                <el-input v-model="form.password" :placeholder="$t('food.shift.password')"></el-input>
+            </el-form-item>
+            <el-form-item label="">
+                <el-button type="primary" @click="save">{{ $t('commons.confirm') }}</el-button>
+            </el-form-item>
+         </el-form>
+     </el-row>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from "vuex";
-
 export default {
+  props:['tabs','employeeList'],
   computed: {
     ...mapState({
       token: (state) => state.user.token,
@@ -158,9 +145,13 @@ export default {
   data() {
     return {
       tabCurr:1,
-      tabs:[],
       loading: false,
-      searchForm:{}
+      searchForm:{},
+      info:{},
+      form:{
+          handoveEmployeedId:'',
+          password:''
+      }
     };
   },
 
@@ -169,32 +160,78 @@ export default {
   },
   methods: {
     initForm() {
-      this.getList();
+
+      this.changeTabs(1);
     },
-    /**获取表格数据 */
-    getList(){
-    	this.loading = false
-    	let params = {}
-    	params.userId = this.userId
-    	params.storesNum = this.storesNum
-        this.$F.doRequest(this, "/pms/handover/list", params, (res) => {
-            console.log(res)
-            this.tabs = res
+    changeTabs(v){
+       this.tabCurr = v
+       if(v == 1){
+           this.getManagerInfo();
+       }
+       if(v == 2){
+           this.getDishesInfo();
+       }
+       if(v == 3){
+           this.getShopInfo();
+       }
+    },
+
+    //前台部交班信息
+    getManagerInfo() {
+        let params = {}
+        params.userId = this.userId
+        params.storesNum = this.storesNum
+        this.$F.doRequest(this, "/pms/handover/handover_base_info", params, (res) => {
+            // console.log(res)
+            this.info = res
         });
     },
-    getDataList() {
-      this.searchForm.token = this.token;
-      this.searchForm.plat_source = this.plat_source;
-      this.searchForm.userId = this.userId;
-      console.log(JSON.stringify(this.searchForm));
-      this.loading = true;
-      enterprise_list(this.searchForm).then((res) => {
-        this.loading = false;
-        if (res.code == 200) {
-          this.tableData = res.data;
-          this.listTotal = res.data.total;
+    //餐饮部交班信息
+    getDishesInfo() {
+        let params = {}
+        params.userId = this.userId
+        params.storesNum = this.storesNum
+        this.$F.doRequest(this, "/pms/dishes/handover/handover_base_info", params, (res) => {
+            console.log(res)
+            this.info = res
+        });
+    },
+    //商店部交班信息
+    getShopInfo() {
+        let params = {}
+        params.userId = this.userId
+        params.storesNum = this.storesNum
+        this.$F.doRequest(this, "/pms/shop/handover/handover_base_info", params, (res) => {
+            console.log(res)
+            this.info = res
+        });
+    },
+
+    save(){
+        let url = '/pms/handover/handover_up'
+        if(this.tabCurr == 1){
+           url = '/pms/handover/handover_up'
         }
-      });
+        if(this.tabCurr == 2){
+           url = '/pms/dishes/handover/handover_up'
+        }
+        if(this.tabCurr == 3){
+            url = '/shop/pms/handover/handover_up'
+        }
+        let params = {
+            workStarTime:this.info.workStarTime,
+            workEndTime:this.info.workEndTime,
+            handoveNum:this.info.handoveNum,
+            handoverStatus:this.info.handoverStatus,
+            handoveEmployeedId:this.form.handoveEmployeedId,
+            password:this.form.password
+        }
+        params.userId = this.userId
+        params.storesNum = this.storesNum
+        this.$F.doRequest(this, url, params, (res) => {
+           console.log(res)
+        });
+
     },
 
     /**多选 */
